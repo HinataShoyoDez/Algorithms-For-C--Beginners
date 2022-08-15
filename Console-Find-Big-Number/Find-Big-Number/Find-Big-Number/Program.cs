2545429bb096d947abdad2158f7c210816df9ef9
AliEnginSivas <108088859+AliEnginSivas@users.noreply.github.com>
2022-08-15 10:50:40 +0300

    Add files via upload

---
+++ Console-Find-Big-Number/Find-Big-Number/Find-Big-Number/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+
+namespace FindBigNumber
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+           
+            Console.WriteLine("Console-Find-Big-Number\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                
+                string numInput1 = "";
+                string numInput2 = "";
+                int result = 0;
+
+                Console.Write("Birinci Sayıyı Girin: ");
+                numInput1 = Console.ReadLine();
+
+                int cleanNum1 = 0;
+                while (!int.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Girdiğiniz sayı tam sayı değil: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                
+                
+                Console.Write("İkinci Sayıyı Girin: ");
+                numInput2 = Console.ReadLine();
+
+                int cleanNum2 = 0;
+                while (!int.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Girdiğiniz sayı tam sayı değil: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                if (cleanNum1<cleanNum2)
+                {
+                    Console.WriteLine("Girdiğiniz ikinci sayı büyüktür");
+                }
+                else if (cleanNum1==cleanNum2)
+                {
+                    Console.WriteLine("Girdiğiniz sayılar Eşittir");
+                }
+                else
+                {
+                    Console.WriteLine("Girdiğiniz birinci sayı büyüktür");
+                }
+
+                Console.Write("Uygulamayı kapatmak için 'n' ve Enter'a basın veya devam etmek için başka bir tuşa ve Enter'a basın: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); 
+            }
+            return;
+        }
+    }
+}
---
+++ Console-Find-Number-of-Digits/Find-Number-of-Digits/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+
+        Console.WriteLine("Console-Find-Number-of-Digits\r");
+        Console.WriteLine("------------------------\n");
+
+        while (!endApp)
+        {
+
+            string numInput1 = "";
+          
+            double result = 0;
+
+
+            Console.Write("Basamak sayısını öğrenmek için sayıyı giriniz: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("Girdiğiniz Değer bir sayı değildir: ");
+                numInput1 = Console.ReadLine();
+            }
+           
+            if ((cleanNum1 >= 0 && cleanNum1 <= 9))
+            {
+                Console.WriteLine("Sayı bir basamaklı");
+            }
+            else if (cleanNum1 >= 10 && cleanNum1 <= 99)
+            {
+                Console.WriteLine("Sayı iki basamaklı");
+            }
+            else if (cleanNum1 >= 100 && cleanNum1 <= 999)
+            {
+                Console.WriteLine("Sayı üç basamaklı");
+            }
+            else if (cleanNum1 >= 1000 && cleanNum1 <= 9999)
+            {
+                Console.WriteLine("Sayı dört basamaklı");
+            }
+            else if (cleanNum1 >= 10000 && cleanNum1 <= 99999)
+            {
+                Console.WriteLine("Sayı beş basamaklı");
+            }
+           
+
+            Console.WriteLine("------------------------\n");
+
+
+            Console.Write("Uygulamayı kapatmak için 'n' ve Enter'a basın veya devam etmek için başka bir tuşa ve Enter'a basın:");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n");
+        }
+        return;
+    }
+}
