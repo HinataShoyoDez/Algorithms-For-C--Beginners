9e9ee866107ff9a543813be5b547e97ac15bfc51
AliEnginSivas <108088859+AliEnginSivas@users.noreply.github.com>
2022-08-15 10:28:22 +0300

    Add files via upload

---
+++ Console-Worker-Salary-Calculation/Worker-Salary-Calculation/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+
+        Console.WriteLine("Console-Worker-Salary-Calculation\r");
+        Console.WriteLine("------------------------\n");
+
+        while (!endApp)
+        {
+
+            string Salary = "";
+            string Kids = "";
+            double result = 0;
+
+
+            Console.Write("Maaş giriniz: ");
+            Salary = Console.ReadLine();
+
+            double cleanSalary = 0;
+            while (!double.TryParse(Salary, out cleanSalary))
+            {
+                Console.Write("Girdiğiniz Değer bir sayı değildir: ");
+                Salary = Console.ReadLine();
+            }
+
+
+            Console.Write("Çocuk sayısı giriniz: ");
+            Kids = Console.ReadLine();
+
+            int cleankids = 0;
+            while (!int.TryParse(Kids, out cleankids))
+            {
+                Console.Write("Girdiğiniz Değer bir sayı değildir: ");
+                Kids = Console.ReadLine();
+            }
+
+            if (cleankids==0)
+            {
+                Console.WriteLine("aldığınız ücret : " + Salary);
+            }
+            else if (cleankids==1)
+            {
+               result = cleanSalary + (cleanSalary * 5 / 100);
+               Console.WriteLine("aldığınız ücret : " + result);
+            }
+            else if (cleankids==2)
+            {
+                result = cleanSalary + (cleanSalary * 10 / 100);
+                Console.WriteLine("aldığınız ücret : " + result);
+            }
+            else if (cleankids>=3)
+            {
+                result = cleanSalary + (cleanSalary * 15 / 100);
+                Console.WriteLine("aldığınız ücret : " + result);
+            }
+
+
+            Console.WriteLine("------------------------\n");
+
+
+            Console.Write("Uygulamayı kapatmak için 'n' ve Enter'a basın veya devam etmek için başka bir tuşa ve Enter'a basın:");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n");
+        }
+        return;
+    }
+}
+
---
+++ N-Number-Of-Squares-And-Sum-And-Product/N-Number-Of-Squares-And-Sum-And-Product/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool endApp = false;
+
+        Console.WriteLine("Console-N-Number-Of-Squares-And-Sum-And-Product\r");
+        Console.WriteLine("------------------------\n");
+
+        while (!endApp)
+        {
+
+            string numInput1 = "";
+            
+            double result = 0;
+            double result2 = 1;
+            double result3= 1;
+            Console.Write("Sayıyı Giriniz: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while (!double.TryParse(numInput1, out cleanNum1))
+            {
+                Console.Write("Girdiğiniz Değer bir sayı değildir: ");
+                numInput1 = Console.ReadLine();
+            }
+
+            for (int i = 1; i <= cleanNum1; i++)
+            {
+                if (i%2==1)
+                {
+                    result += i;
+                    result2 *= i;
+                }
+                else
+                {
+                    result3 *= i;
+                }
+                
+            }
+            Console.WriteLine("Tek sayıların toplamı : " + result);
+            Console.WriteLine("Tek sayıların Çarpımı : " + result2);
+            Console.WriteLine("Çift sayıların çarpımı : " + result3);
+
+            Console.Write("Uygulamayı kapatmak için 'n' ve Enter'a basın veya devam etmek için başka bir tuşa ve Enter'a basın:");
+            if (Console.ReadLine() == "n") endApp = true;
+
+            Console.WriteLine("\n");
+        }
+        return;
+    }
+}
